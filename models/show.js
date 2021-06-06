904901cd86cad5dd98579bf548347ebaaa357481
stark9147 <pillaidayakumar@gmail.com>
2021-06-06 23:04:36 +0530

    added booking functionality

--- models/show.js
+++ models/show.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const showSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    time: {
-        type: Date,
-        required: true,
-    },
-    genre: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    type: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    ticketsRemaining: {
-        type: Number,
-        required: true,
-        default: 15
-    },
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ticketsRemaining: {
+    type: Number,
+    required: true,
+    default: 15,
+  },
+});
 
-showSchema.pre('findOneAndUpdate', function (next) {
-    this.options.runValidators = true
-    next()
-})
+showSchema.pre("findOneAndUpdate", function (next) {
+  this.options.runValidators = true;
+  next();
+});
 
-
-const Show = mongoose.model('show', showSchema)
+const Show = mongoose.model("show", showSchema);
 
 module.exports = Show;
